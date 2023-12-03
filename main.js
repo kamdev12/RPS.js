3c45f1dc3a0ef39ac1b2fbca38a98d731c0c5251
Kamal Graham <kamalgraham187@gmail.com>
2023-12-03 00:28:50 -0500

    Created the function to create round and play a round with a predefined player option

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log("Hello World!");
 const options = ["rock", "paper", "scissors"];
 
 const getComputerChoice = () => {
-    let choice = Math.floor(Math.random() * 4);
+    let choice = Math.floor(Math.random() * 3) + 1;
     
 
     switch (choice){
@@ -16,12 +16,33 @@ const getComputerChoice = () => {
         case 3:
             return "scissors";
             break;
-            default :
-                return "There was an unexpected error."
-            break;
+            
     }
 
     console.log(choice);
 }
 
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
+
+const playRound = (playerSelection, computerSelection) =>{
+    
+
+    if (playerSelection == computerSelection){
+        return "There was a tie, replay round"
+    }
+    else if (
+        (playerSelection == "rock" && computerSelection == "scissors")||
+        (playerSelection == "paper" && computerSelection == "rock")||
+        (playerSelection == "scissors" && computerSelection == "paper")
+    ){
+        return `You Won! ${playerSelection} beats ${computerSelection}`;
+    }
+    else{
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    
+}
+
+const playerSelection = "rock"
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
